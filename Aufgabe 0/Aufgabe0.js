62bb4d9d9a08b33e460b5cc407002a4fd1c96c91
lorena <lorena@192.168.0.235>
2017-10-11 17:46:44 +0200

    Aufgabe 0 fertig gestellt

--- Aufgabe 0/Aufgabe0.js
+++ Aufgabe 0/Aufgabe0.js
@@ -2,15 +2,17 @@
 Aufgabe: Aufgabe 0 - Start: Git, Github und Pages
 Name: Lorena Eberhart
 Matrikel: 256328
-Datum: 10.10.17
+Datum: 11.10.17
     
 Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe.
 Er wurde nicht kopiert und auch nicht diktiert.
 
 */
-window.prompt("What´s your name?", "");
-if (name != null) {
-    document.getElementById("test").innerHTML =
-        "Hello " + name + "! Nice to meet you!";
+function hallo() {
+    let name = prompt("What´s your name?", "");
+    if (name != null) {
+        document.getElementById("test").innerHTML =
+            "Hello " + name + "! Nice to meet you!";
+    }
 }
 //# sourceMappingURL=Aufgabe0.js.map
--- Aufgabe 0/Aufgabe0.ts
+++ Aufgabe 0/Aufgabe0.ts
@@ -2,7 +2,7 @@
 Aufgabe: Aufgabe 0 - Start: Git, Github und Pages
 Name: Lorena Eberhart
 Matrikel: 256328
-Datum: 10.10.17
+Datum: 11.10.17
     
 Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. 
 Er wurde nicht kopiert und auch nicht diktiert.
@@ -10,9 +10,12 @@ Er wurde nicht kopiert und auch nicht diktiert.
 */
 
 
-window.prompt("What´s your name?", "");
+function hallo() {
+
+let name = prompt("What´s your name?", "");
 
 if (name != null) {
     document.getElementById("test").innerHTML =
     "Hello " + name + "! Nice to meet you!";
 }
+    }

bd0e89cfc49f7b26bdde5e1e00204832d05d476c
lorena <lorena@lorenas-mbp>
2017-12-17 14:36:21 +0100

    Aufgabe 9 fertig

--- Aufgabe9/Aufgabe9.ts
+++ Aufgabe9/Aufgabe9.ts
@@ -14,7 +14,8 @@ namespace A9 {
     window.addEventListener("load", init);
     let currentLetter: string;
 
-
+    //document.addEventListener("keypressed", handleAlt);
+    document.addEventListener ("keydown", tastaturEingabe);
 
     //Buchstaben erstellen
     function init(): void {
@@ -59,7 +60,7 @@ namespace A9 {
         let div: HTMLDivElement = document.createElement("div");
 
         div.innerText = _buchstaben;
-        div.style.backgroundColor = "red";
+        div.style.backgroundColor = "lightblue";
         div.style.width = "35px";
         div.style.height = "35px";
         div.style.marginLeft = "2px";
@@ -101,7 +102,7 @@ namespace A9 {
         let box: HTMLDivElement = document.createElement("div");
         
         box.innerText =currentLetter;
-        box.style.backgroundColor ="red";
+        box.style.backgroundColor ="lightblue";
         box.style.width = "35px";
         box.style.height = "35px";
         box.style.marginLeft = "2px";
@@ -112,10 +113,169 @@ namespace A9 {
         box.style.left = _event.pageX + "px";
         box.style.top= _event.pageY + "px";
         
-        box.addEventListener("click", setLetters);
+        box.addEventListener("click", deleteLetter);
         document.body.appendChild(box);
         
           let clicking: HTMLDivElement = <HTMLDivElement>_event.target;
     }
+    
+    //Buchstaben mit Tastatur auswählen
+    function tastaturEingabe(event: KeyboardEvent): void {
+        
+        if (event.key == "a" || event.key == "A") { 
+            currentLetter = event.key; 
+            setLetters;
+        }
+        
+        else if (event.key == "b" || event.key == "B") {
+            currentLetter = event.key;
+            setLetters;
+        }
+        
+        else if (event.key == "c" || event.key == "C") {
+            currentLetter = event.key;
+            setLetters;
+        }
+
+        else if (event.key == "d" || event.key == "D") {
+            currentLetter = event.key;
+            setLetters;
+        }
+
+        else if (event.key == "e" || event.key == "E") {
+            currentLetter = event.key;
+            setLetters;
+        }
+
+        else if (event.key == "f" || event.key == "F") {
+            currentLetter = event.key;
+            setLetters;
+        }
+
+        else if (event.key == "g" || event.key == "G") {
+            currentLetter = event.key;
+            setLetters;
+        }
+
+        else if (event.key == "h" || event.key == "H") {
+            currentLetter = event.key;
+            setLetters;
+        }
+
+        else if (event.key == "i" || event.key == "I") {
+            currentLetter = event.key;
+            setLetters;
+        }
+
+        else if (event.key == "j" || event.key == "J") {
+            currentLetter = event.key;
+            setLetters;
+        }
+
+        else if (event.key == "k" || event.key == "K") {
+            currentLetter = event.key;
+            setLetters;
+        }
+
+        else if (event.key == "l" || event.key == "L") {
+            currentLetter = event.key;
+            setLetters;
+        }
+
+        else if (event.key == "m" || event.key == "M") {
+            currentLetter = event.key;
+            setLetters;
+        }
+
+        else if (event.key == "n" || event.key == "N") {
+            currentLetter = event.key;
+            setLetters;
+        }
+
+        else if (event.key == "o" || event.key == "O") {
+            currentLetter = event.key;
+            setLetters;
+        }
+
+        else if (event.key == "p" || event.key == "P") {
+            currentLetter = event.key;
+            setLetters;
+        }
+
+        else if (event.key == "q" || event.key == "Q") {
+            currentLetter = event.key;
+            setLetters;
+        }
+
+        else if (event.key == "r" || event.key == "R") {
+            currentLetter = event.key;
+            setLetters;
+        }
+
+        else if (event.key == "s" || event.key == "S") {
+            currentLetter = event.key;
+            setLetters;
+        }
+
+        else if (event.key == "t" || event.key == "T") {
+            currentLetter = event.key;
+            setLetters;
+        }
+
+        else if (event.key == "u" || event.key == "U") {
+            currentLetter = event.key;
+            setLetters;
+        }
+
+        else if (event.key == "v" || event.key == "V") {
+            currentLetter = event.key;
+            setLetters;
+        }
+
+        else if (event.key == "w" || event.key == "W") {
+            currentLetter = event.key;
+            setLetters;
+        }
+
+        else if (event.key == "x" || event.key == "X") {
+            currentLetter = event.key;
+            setLetters;
+        }
+
+        else if (event.key == "y" || event.key == "Y") {
+            currentLetter = event.key;
+            setLetters;
+        }
+
+        else if (event.key == "z" || event.key == "Z") {
+            currentLetter = event.key;
+            setLetters;
+        }
+
+
+    //document.addEventListener ("keydown", tastaturEingabe);
+
+
+    }
+
+    
+    
+    
+    
+    //ALt gedrückt halten
+    function handleAlt (_event: KeyboardEvent): void {
+        if(_event.keyCode ==18) {
+            
+            deleteLetter;
+            }
+        }
+    
+    function deleteLetter(_event:MouseEvent): void {
+        
+        let d: HTMLDivElement = <HTMLDivElement> _event.target;
+        document.addEventListener("keypressed", handleAlt);
+        document.body.removeChild(d);
+        }
+  
 
 }

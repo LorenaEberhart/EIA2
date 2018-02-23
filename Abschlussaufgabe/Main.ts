a68a12d540291ff1608e71f9e9c7653b5923a4ab
lorena <lorena@lorenas-mbp>
2018-02-23 14:33:10 +0100

    Änderung

--- Abschlussaufgabe/Main.ts
+++ Abschlussaufgabe/Main.ts
@@ -12,7 +12,7 @@ Er wurde nicht kopiert und auch nicht diktiert.
 namespace Abschluss {
      window.addEventListener("load", init);
     
-   
+   alert("Helfe Deutschland neue Länderfreundschaften zu bilden. \nKlicke dazu zunächst die deutsche Flagge an und danach die Flagge deiner Wahl.");
     
     
     let currentFlagOne: string;
